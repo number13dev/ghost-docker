ddbe6b3053cfd8c52fa32f4ae82a1c56e23bf426
Johannes <johannes.kuehnel@number13.de>
2018-03-26 19:09:35 +0200

    asdfA

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-echo "#########  running install  #########"
-wget https://github.com/TryGhost/Ghost/releases/download/0.11.9/Ghost-0.11.9.zip
+echo "~~~~~~~~~~~~~~ running install ~~~~~~~~~~~~~~"
+wget https://github.com/TryGhost/Ghost/releases/download/0.11.9/Ghost-0.11.9.zip -O ghost.zip
 mkdir -p /var/www/ghost
 chown -R www:www /var/lib/nginx
 chown -R www:www /var/www
@@ -9,7 +9,7 @@ unzip -o ghost.zip -d /var/www/ghost
 cd /var/www/ghost
 pwd
 npm install --production
-npm install -g knex-migrator
 cd /
 
-echo "#########  INSTALL DONE  #########"
+
+echo "~~~~~~~~~~~~~~ ghost unzip done, install done ~~~~~~~~~~~~~~"
